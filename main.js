482f054dbbde405f1282c58dc8f87b9da335a8f3
Briseida Montiel <brizism@gmail.com>
2017-10-25 00:01:57 -0400

    get value of input

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 // Get input element and add event listener
 let filterInput = document.getElementById('filterInput').addEventListener('keyup', filterNames);
 
+function filterNames(){
+    // Get value of input
+    let filterValue = document.getElementById('filterInput').value.toUpperCase();
+    
+    
+}
