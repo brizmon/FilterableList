89f497533656d852576b83bf247d53f79e6d587d
Briseida Montiel <brizism@gmail.com>
2017-10-25 00:09:54 -0400

    toUpperCase

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function filterNames(){
     for(let i = 0; i < li.length; i++){
         let a = li[i].getElementsByTagName('a')[0];
         // If matched
-        if(a.innerHTML.toLocaleUpperCase().indexOf(filterValue) > -1){
+        if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
             li[i].style.display = '';
         } else {
             li[i].style.display = 'none';
