99217313e9c93ae18271139c44cb5bfc85a8ec52
Briseida Montiel <brizism@gmail.com>
2017-10-25 00:04:09 -0400

    loop through li's

--- main.js
+++ main.js
@@ -10,5 +10,14 @@ function filterNames(){
     // Get lis from ul
     let li = ul.querySelectorAll('li.collection-item');
 
-    
+    // Loop through collection-item lis
+    for(let i = 0; i < li.length; i++){
+        let a = li[i].getElementsByTagName('a')[0];
+        // If matched
+        if(a.innerHTML.toLocaleUpperCase().indexOf(filterValue) > -1){
+            li[i].style.display = '';
+        } else {
+            li[i].style.display = 'none';
+        }
+    }
 }
